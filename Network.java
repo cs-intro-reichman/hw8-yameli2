dbbcf948fe80c245c8148836c9dbc5f31995ee0a
yameli2 <yam.elimelech@post.runi.ac.il>
2025-01-09 01:31:22 +0200

    T

--- Network.java
+++ Network.java
@@ -131,7 +131,7 @@ public class Network {
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
        //// Replace the following statement with your code
-       String usersDiscription="Network: ";
+       String usersDiscription="Network:";
        for(int i =0;i<userCount;i++)
        {
             usersDiscription = usersDiscription + "\n"+ users[i].toString();
